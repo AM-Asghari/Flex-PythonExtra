706a51b5b99589cc2aac0fb0488b9ad92aabaddd
mo <mosawer.asg@outlook.com>
2021-10-01 14:49:07 +0200

    turtle

---
+++ 01-Turtle/myTurtle.py
@@ -0,0 +1,14 @@
+import turtle             
+
+turtle.speed(0)
+for x in range(10):
+   turtle.forward(66)
+   turtle.right(95)
+   turtle.forward(66)
+   turtle.right(95)
+   turtle.forward(66)
+   turtle.right(95)
+   turtle.forward(66)
+   turtle.right(95)
+   
+turtle.done()
